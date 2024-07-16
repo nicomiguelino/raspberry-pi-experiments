67bfeceb9c7e055320d8f83cd2b86ab9dc00aab4
nicomiguelino <nicomiguelino2014@gmail.com>
2024-07-16 15:07:05 -0700

    fix script

--- vlc/scripts/create_dockerfile.sh
+++ vlc/scripts/create_dockerfile.sh
@@ -2,7 +2,10 @@
 
 set -euo pipefail
 
-export IMAGE_NAME=${IMAGE_NAME+x:raspberrypi3-debian}
-export IMAGE_TAG=${DOCKER_IMAGE_TAG+x:bookworm}
+export IMAGE_NAME=${IMAGE_NAME:-raspberrypi3-debian}
+export IMAGE_TAG=${IMAGE_TAG:-bookworm}
+
+echo "IMAGE_NAME: $IMAGE_NAME"
+echo "IMAGE_TAG: $IMAGE_TAG"
 
 cat docker/Dockerfile.template | envsubst > docker/Dockerfile
